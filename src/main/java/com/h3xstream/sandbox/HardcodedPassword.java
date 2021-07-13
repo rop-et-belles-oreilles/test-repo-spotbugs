76651e1921633e2b7de23a8a9b47e12eda90fdb0
rop-et-belles-oreilles <49708544+rop-et-belles-oreilles@users.noreply.github.com>
2021-07-13 09:28:03 -0400

    Add new file with vulnerability

---
+++ src/main/java/com/h3xstream/sandbox/HardcodedPassword.java
@@ -0,0 +1,11 @@
+package com.h3xstream.sandbox;
+
+import java.util.Properties;
+
+public class HardcodedPassword {
+    public static void main(String[] args) {
+        Properties p = new Properties();
+        p.setProperty("user","admin");
+        p.setProperty("password","1234");
+    }
+}
